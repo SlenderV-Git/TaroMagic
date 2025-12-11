07b7c3ed1e2df29f0e3f8a96cafc031682d46459
Viktor Bagrov <keramogranitus@gmail.com>
2025-12-11 16:08:54 +0300

    add user service

--- src/services/gateway.py
+++ src/services/gateway.py
@@ -1,3 +1,4 @@
+from src.services.user import UserService
 from src.common.interfaces.gateway import BaseGateway
 from src.database.gateway import DBGateway
 
@@ -9,5 +10,8 @@ class ServicesGateway(BaseGateway):
         self._database = database
         super().__init__(database)
 
+    def name(self) -> UserService:
+        return UserService(repository=self._database.user())
+    
     #def name(self) -> NameService:
     #    return NameService(repository=self._database.name())
