3d526ab5929c53666cbfba4378ca50fc0e1d3315
Viktor Bagrov <keramogranitus@gmail.com>
2025-12-11 21:12:41 +0300

    connect webhook

--- src/bot/webhook.py
+++ src/bot/webhook.py
@@ -12,7 +12,7 @@ update_router = APIRouter(tags=["webhook"])
 @update_router.post("/webhook")
 async def handle_update(
     update : Update, 
-    bot : Annotated[Bot, Depends(Bot())],
+    bot : Annotated[Bot, Depends()],
     dp : Annotated[Dispatcher, Depends(get_root_dispather)]
     ):
     await dp.feed_update(bot, update)
