74d5f8f75ea4dc9c6729c774fc9532678c38d4e4
Viktor Bagrov <keramogranitus@gmail.com>
2025-12-12 22:14:57 +0300

    Fix update transport

--- src/bot/webhook.py
+++ src/bot/webhook.py
@@ -5,7 +5,6 @@ from aiogram.types import Update
 from aiogram import Bot, Dispatcher
 
 from src.api.common.providers.stub import Stub
-from src.bot.handlers import get_root_dispather
 
 
 update_router = APIRouter(tags=["webhook"])
